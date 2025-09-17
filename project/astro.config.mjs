2217b11962920ff82d587ac017033b0749bbca9b
Patricio Brito <patriciobritodesign@gmail.com>
2025-09-17 23:45:10 +0100

    update all

---
+++ project/astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  server: {
+    hmr: {
+      clientPort: 96435430,
+      host: true
+    }
+  }
+});
